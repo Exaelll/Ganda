82d398454ecf19c7cd63c1ca8e201bd209c841b2
Exael <126078477+Exaelll@users.noreply.github.com>
2024-05-14 00:46:51 +0800

    Update HiCrush.js

--- HiCrush.js
+++ HiCrush.js
@@ -1,19 +1,40 @@
+// GO TO COLLAGE
+
+function password() {
+    document.getElementById("login").style.display = "flex";
+}
+
+document.getElementById("loginForm").addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent form submission
+
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    if (username === "Ganda" && password === "GandaGandaGanda") {
+        window.location.href = "HiKyut.html";
+
+    } else {
+        alert("Invalid username or password. Please try again.");
+    }
+});
+
+
 // SLIDESHOW
 
 let slideIndex = 0;
-    showSlides();
-    
-    function showSlides() {
-      let i;
-      let slides = document.getElementsByClassName("mySlides");
-      for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
-      }
-      slideIndex++;
-      if (slideIndex > slides.length) {slideIndex = 1}    
-      slides[slideIndex-1].style.display = "block";  
-      setTimeout(showSlides, 1250); // Change image every 1.5 seconds
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 1250); // Change image every 1.5 seconds
+}
 
 //---------------------------------------------------------------
 
@@ -70,20 +91,20 @@ nBtn.addEventListener("mouseover", function (event) {
 
 //---------------------------------------------------------------
 
-  //YES BUTTON
-  // EVERYTHING SHOULD BE HIDDEN AND THEN SHOW THE YES OUTCOME
+//YES BUTTON
+// EVERYTHING SHOULD BE HIDDEN AND THEN SHOW THE YES OUTCOME
 const btn = document.getElementById('yBtn');
 
 btn.addEventListener('click', () => {
-    
-  const box = document.getElementsByClassName('yesOutcome')[0];
-  // 👇️ hides element (still takes up space on the page)
-  // box.style.visibility = 'hidden';
 
-  // 👇️ removes the element from the DOM
+    const box = document.getElementsByClassName('yesOutcome')[0];
+    // 👇️ hides element (still takes up space on the page)
+    // box.style.visibility = 'hidden';
+
+    // 👇️ removes the element from the DOM
     box.style.display = 'block';
 
-//removes the question
+    //removes the question
     const theQuestion = document.getElementsByClassName('Yess')[1];
     theQuestion.style.display = 'none';
 
@@ -93,8 +114,8 @@ btn.addEventListener('click', () => {
 
 function openNav() {
     document.getElementById("myNav").style.width = "100%";
-  }
-  
-  function closeNav() {
+}
+
+function closeNav() {
     document.getElementById("myNav").style.width = "0%";
-  }
+}
