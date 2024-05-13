f3b5dc3656f32a73572c2886e140cbd6519ecabf
Exael <126078477+Exaelll@users.noreply.github.com>
2024-05-14 00:52:39 +0800

    Add files via upload

---
+++ SuperGanda.js
@@ -0,0 +1,7 @@
+function toggleGrid() {
+    var gridContainer = document.querySelector('.wrap');
+    var headerButton = document.querySelector('.Header');
+
+    // Toggle class to move gridContainer
+    gridContainer.classList.toggle('slide-down');
+}
