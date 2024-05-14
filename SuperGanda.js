fd50f14727572c2e29f5d54552af12ec22972000
Exael <126078477+Exaelll@users.noreply.github.com>
2024-05-14 18:50:34 +0800

    Update SuperGanda.js

--- SuperGanda.js
+++ SuperGanda.js
@@ -1,5 +1,5 @@
 function toggleGrid() {
-    var gridContainer = document.querySelector('.wrap');
+    var gridContainer = document.querySelector('.gridContainer');
     var headerButton = document.querySelector('.Header');
 
     // Toggle class to move gridContainer
